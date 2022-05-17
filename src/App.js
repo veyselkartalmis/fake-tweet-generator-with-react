bb8fe159808214d5b0e443d77794b31c5c01776a
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-05-17 12:35:24 +0300

    profile picture and screenshot tool added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, createRef, useEffect } from "react";
 import "./style.scss";
 import { ReplyIcon, RetweetIcon, LikeIcon, ShareIcon, VerifiedIcon } from "./icons";
+import { AvatarLoader } from "./Loader";
+import { useScreenshot } from "use-react-screenshot";
 
-
-// Tweeti ozel karakterlerden ayirarak ayri renklendirme yapabilmemizi saglayan fonksiyon (regEx)
+// Tweeti ozel karakterlerden ayirarak link renklendirmesi yapabilmemizi saglayan fonksiyon (regEx)
 const tweetFormat = tweet => {
     tweet = tweet
         .replace(/@([\w]+)/g, '<span>@$1</span>')
@@ -13,7 +14,20 @@ const tweetFormat = tweet => {
     return tweet;
 }
 
+// Eger istatistikler 1000'den buyuk ise sayıyı parcalamaya yarayan fonksiyon
+const formatNumber = number => {
+    if(!number){number = 0}
+    if(number < 1000) {return number}
+    number /= 1000;
+    number = String(number).split(".");
+    return (
+        number[0] + (number[1] > 100 ? "," + number[1].slice(0, 1) + " B" : " B")
+    )
+}
+
 export default function App() {
+    const tweetRef = createRef();
+    const downloadRef = createRef();
     const [name, setName] = useState();
     const [userName, setUserName] = useState();
     const [isVerified, setIsVerified] = useState(false);
@@ -22,6 +36,22 @@ export default function App() {
     const [retweets, setRetweets] = useState(0);
     const [quoteTweets, setQuoteTweets] = useState(0);
     const [likes, setLikes] = useState(0);
+    const [image, takeScreenshot] = useScreenshot();
+    const getImage = () => takeScreenshot(tweetRef.current);
+
+    useEffect(() => {
+        if(image){downloadRef.current.click();}
+    }, image)
+
+    // Yuklenilen resmin profil resmi olmasi icin
+    const avatarHandle = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.addEventListener('load', function(){
+            setAvatar(this.result);
+        });
+        reader.readAsDataURL(file);    
+    }
 
     return (
         <>
@@ -55,6 +85,13 @@ export default function App() {
                         />
                     </li>
                     <li>
+                        <label>Profil Resmi</label>
+                        <input
+                            type="file"
+                            onChange={avatarHandle}
+                        />
+                    </li>
+                    <li>
                         <label>Retweet Sayısı</label>
                         <input
                             type="number"
@@ -82,12 +119,16 @@ export default function App() {
                             onChange={e => setLikes(e.target.value)}
                         />
                     </li>
+                    <button onClick={getImage}>Tweeti Oluştur ve İndir</button>
+                    <div className="download-url">
+                        {image && (<a ref={downloadRef} href={image} download="tweet.png">Tweeti İndir</a>)}
+                    </div>
                 </ul>
             </div>
             <div className="tweet-container">
-                <div className="tweet">
+                <div className="tweet" ref={ tweetRef }>
                     <div className="tweet-author">
-                        <img src="https://avatars.githubusercontent.com/u/19537228?v=4" alt="user" />
+                        {(avatar && <img src={avatar} alt="user" />) || <AvatarLoader />}
                         <div>
                             <div className="name">
                                 {name || "Ad Soyad"}
@@ -97,6 +138,7 @@ export default function App() {
                         </div>
                     </div>
                     <div className="tweet-content">
+                        {/* JS icerisine HTML injection edebilmek icin gerekli fonksiyon  */}
                         <p dangerouslySetInnerHTML={{
                             __html:
                                 (tweet && tweetFormat(tweet)) ||
@@ -105,9 +147,9 @@ export default function App() {
                         />
                     </div>
                     <div className="tweet-stats">
-                        <span><b>{retweets}</b> Retweet</span>
-                        <span><b>{quoteTweets}</b> Alıntı Tweetler</span>
-                        <span><b>{likes}</b> Beğeni</span>
+                        <span><b>{formatNumber(retweets)}</b> Retweet</span>
+                        <span><b>{formatNumber(quoteTweets)}</b> Alıntı Tweetler</span>
+                        <span><b>{formatNumber(likes)}</b> Beğeni</span>
                     </div>
                     <div className="tweet-actions">
                         <span><ReplyIcon /></span>
